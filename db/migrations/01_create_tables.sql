b20238e38bc226d2bb59fa0cebd8704de08fe8b6
Joe Rybarczyk <joerybarczyk99@gmail.com>
2024-07-28 20:29:31 -0400

    add episode-frame relationship and migration script to load episode data

--- db/migrations/01_create_tables.sql
+++ db/migrations/01_create_tables.sql
@@ -1,6 +1,25 @@
 CREATE TABLE frames (
     id SERIAL PRIMARY KEY,
-    frame_number INT NOT NULL,
-    subtitle TEXT NOT NULL,
+    timestamp INT NOT NULL,
+    episode TEXT NOT NULL,
+    subtitle TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE episodes (
+    id SERIAL PRIMARY KEY,
+    key TEXT NOT NULL,
+    season INT NOT NULL,
+    episode_number INT NOT NULL,
+    title TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Ensure the key field in the episodes table is unique
+ALTER TABLE episodes
+ADD CONSTRAINT unique_key UNIQUE (key);
+
+-- Add a foreign key constraint to the frames table
+ALTER TABLE frames
+ADD CONSTRAINT fk_episode
+FOREIGN KEY (episode) REFERENCES episodes(key);
