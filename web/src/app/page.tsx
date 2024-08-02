f68e290c52ed3d3040335fc2209fdff5dc51635d
Joe Rybarczyk <joerybarczyk99@gmail.com>
2024-08-01 21:33:44 -0400

    refactor: React component refactor

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,11 +1,23 @@
-import Results from "./ui/Results";
+import SearchResult from "./ui/SearchResult";
 
-export default function Home({ searchParams}: { searchParams: { q?: string; } }) {
+async function search(query: string): Promise<any> {
+  const response = await fetch(`${process.env.API_HOST}/search?q=${query}`);
+  return response.json();
+}
+
+export default async function Home({ searchParams}: { searchParams: { q?: string; } }) {
   const query = searchParams?.q || '';
+  const data = await search(query);
 
   return (
-      <div className="flex justify-center">
-        <Results query={query} />
+    <div className="flex justify-center">
+      <div className="grid grid-cols-3 gap-2">
+        {data.map((result: any, i: number) => (
+          <div key={result.timestamp + result.episode + i} className="p-1">
+            <SearchResult result={result} />
+          </div>
+        ))}
       </div>
+    </div>
   );
 }
