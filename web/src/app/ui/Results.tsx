47b70c3c35735a70da5f2333e19c23f3d109195f
Joe Rybarczyk <joerybarczyk99@gmail.com>
2024-08-01 21:08:10 -0400

    feat(web) use Next image library for search results

--- web/src/app/ui/Results.tsx
+++ web/src/app/ui/Results.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 async function search(query: string): Promise<any> {
     const response = await fetch(`${process.env.API_HOST}/search?q=${query}`);
     return response.json();
@@ -14,8 +16,8 @@ export default async function Results({ query }: { query: string }) {
         <div className="grid grid-cols-3 gap-2">
             {data.map((result: any, i: number) => (
                 <div key={result.timestamp + result.episode + i} className="p-1">
-                    <img key={result.frame} src={getImgUrl(result)} className="box-border hover:outline hover:outline-8 hover:outline-theme-black"/>
-                </div>
+                    <Image src={getImgUrl(result)} alt={`${result.episode}: ${result.timestamp}`} width={400} height={240} className="box-border hover:outline hover:outline-8 hover:outline-theme-black" />
+               </div>
             ))}
         </div>
     );
