f4b6b777ff6ca3191cd6437b31b4f638ffdde035
Joe Rybarczyk <joerybarczyk99@gmail.com>
2024-08-10 12:20:40 -0400

    feat(api): add cors middleware

--- api/cmd/api/main.go
+++ api/cmd/api/main.go
@@ -9,42 +9,50 @@ import (
 	"github.com/elastic/go-elasticsearch/v7"
 	"github.com/gorilla/mux"
 	_ "github.com/lib/pq"
+	"github.com/rs/cors"
 
 	"github.com/99xtal/bluthinator/api/internal/config"
 	"github.com/99xtal/bluthinator/api/internal/handlers"
 )
 
 func main() {
-    config := config.New()
+	config := config.New()
 
-    // Initialize the Elasticsearch client
+	// Initialize the Elasticsearch client
 	var err error
 	esClient, err := elasticsearch.NewClient(config.GetElasticSearchConfig())
-    if err != nil {
-        log.Fatalf("Error creating the client: %s", err)
-    }
+	if err != nil {
+		log.Fatalf("Error creating the client: %s", err)
+	}
 
-    // Initialize the PostgreSQL database connection
-    connStr := config.GetPostgresConnString()
-    fmt.Println(connStr)
-    db, err := sql.Open("postgres", connStr)
-    if err != nil {
-        log.Fatalf("Error connecting to the database: %s", err)
-    }
-    defer db.Close()
+	// Initialize the PostgreSQL database connection
+	connStr := config.GetPostgresConnString()
+	fmt.Println(connStr)
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatalf("Error connecting to the database: %s", err)
+	}
+	defer db.Close()
 
-    server := handlers.NewServer(db, esClient)
+	server := handlers.NewServer(db, esClient)
 
 	router := mux.NewRouter()
 
 	// Routes
-    router.HandleFunc("/episode/{key}", server.GetEpisodeData).Methods("GET")
-    router.HandleFunc("/episode/{key}/{timestamp}", server.GetEpisodeFrame).Methods("GET")
+	router.HandleFunc("/episode/{key}", server.GetEpisodeData).Methods("GET")
+	router.HandleFunc("/episode/{key}/{timestamp}", server.GetEpisodeFrame).Methods("GET")
 	router.HandleFunc("/nearby", server.GetNearbyFrames).Methods("GET")
 	router.HandleFunc("/search", server.SearchFrames).Methods("GET")
 
+	// Configure CORS
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000"},
+		AllowCredentials: true,
+	})
+	handler := c.Handler(router)
+
 	// Start the server
-    port := ":" + config.ServerPort
+	port := ":" + config.ServerPort
 	log.Println("Server listening on port", port)
-	log.Fatal(http.ListenAndServe(port, router))
+	log.Fatal(http.ListenAndServe(port, handler))
 }
--- api/go.mod
+++ api/go.mod
@@ -5,6 +5,7 @@ go 1.20
 require (
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/lib/pq v1.10.9 // indirect
+	github.com/rs/cors v1.11.0 // indirect
 )
 
 require github.com/elastic/go-elasticsearch/v7 v7.17.10
