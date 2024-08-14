de133bb86a8076c6c7d23f978f534ce41c39d0b5
Joe Rybarczyk <joerybarczyk99@gmail.com>
2024-08-13 21:37:13 -0400

    feat(api): implement caption handler

--- api/go.mod
+++ api/go.mod
@@ -3,9 +3,12 @@ module github.com/99xtal/bluthinator/api
 go 1.20
 
 require (
+	github.com/fogleman/gg v1.3.0 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/lib/pq v1.10.9 // indirect
 	github.com/rs/cors v1.11.0 // indirect
+	golang.org/x/image v0.19.0 // indirect
 )
 
 require github.com/elastic/go-elasticsearch/v7 v7.17.10
--- api/internal/handlers/caption.go
+++ api/internal/handlers/caption.go
@@ -1,9 +1,15 @@
 package handlers
 
 import (
+	"bytes"
+	"encoding/base64"
 	"fmt"
+	"image"
+	"image/png"
 	"net/http"
+	"strings"
 
+	"github.com/fogleman/gg"
 	"github.com/gorilla/mux"
 )
 
@@ -13,13 +19,85 @@ func (s *Server) GetCaptionedFrame(w http.ResponseWriter, r *http.Request) {
 	key := params["key"]
 	timestamp := params["timestamp"]
 
+	base64Caption := r.URL.Query().Get("b")
+
+	// Decode the base64-encoded caption
+	captionBytes, err := base64.StdEncoding.DecodeString(base64Caption)
+	if err != nil {
+		http.Error(w, "Error decoding the caption", http.StatusBadRequest)
+		return
+	}
+	caption := string(captionBytes)
+
 	data, err := s.ObjectStorage.GetObject(fmt.Sprintf("bluthinator/frames/%s/%s/medium.png", key, timestamp))
 	if err != nil {
 		http.Error(w, "Error fetching the image", http.StatusInternalServerError)
 		return
 	}
 
+	img, _, err := image.Decode(bytes.NewReader(data))
+	if err != nil {
+		http.Error(w, "Error decoding the image", http.StatusInternalServerError)
+		return
+	}
+
+	// Prepare drawing context
+	dc := gg.NewContextForImage(img)
+	imgWidth := float64(dc.Width())
+	imgHeight := float64(dc.Height())
+
+	fontColor := [3]float64{0.97254, 0.89803, 0.67843}
+	fontSize := float64(32)
+	if err := dc.LoadFontFace("static/fonts/DeFonteReducedNormal.ttf", fontSize); err != nil {
+		panic(err)
+	}
+
+	splitTextIntoLines := func(text string, maxWidth float64) []string {
+		words := strings.Fields(text)
+		var lines []string
+		var currentLine string
+		for _, word := range words {
+			testLine := currentLine + " " + word
+			testWidth, _ := dc.MeasureString(testLine)
+			if testWidth > maxWidth {
+				lines = append(lines, strings.TrimSpace(currentLine))
+				currentLine = word
+			} else {
+				currentLine = testLine
+			}
+		}
+		lines = append(lines, strings.TrimSpace(currentLine))
+		return lines
+	}
+
+	lines := splitTextIntoLines(caption, imgWidth-20) // 20 pixels padding
+
+	// Draw each line of text
+	lineHeight := fontSize * 1.2
+	startY := imgHeight - float64(len(lines))*lineHeight - 48 // 10 pixels padding from the bottom
+
+	for i, line := range lines {
+		y := startY + float64(i)*lineHeight
+
+		// Draw the shadow with reduced opacity
+		shadowColor := [3]float64{0, 0, 0} // Black color for the shadow
+		shadowOpacity := 0.5               // 50% opacity
+		shadowOffsetX := 2.0
+		shadowOffsetY := 2.0
+
+		dc.SetRGBA(shadowColor[0], shadowColor[1], shadowColor[2], shadowOpacity)
+		dc.DrawStringAnchored(line, imgWidth/2+shadowOffsetX, y+shadowOffsetY, 0.5, 0.5)
+
+		// Draw the main text
+		dc.SetRGB(fontColor[0], fontColor[1], fontColor[2])
+		dc.DrawStringAnchored(line, imgWidth/2, y, 0.5, 0.5)
+	}
+
 	// Write the response
 	w.Header().Set("Content-Type", "image/png")
-	w.Write(data)
+	err = png.Encode(w, dc.Image())
+	if err != nil {
+		http.Error(w, "Error encoding the image", http.StatusInternalServerError)
+		return
+	}
 }
