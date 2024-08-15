920759f31b79b7cd69ac975e3d15e59b786b1be7
Joe Rybarczyk <joerybarczyk99@gmail.com>
2024-08-14 21:45:15 -0400

    feat(web): add nav arrows to FrameSelector component

--- web/src/app/components/NearbySelector.tsx
+++ web/src/app/components/NearbySelector.tsx
@@ -2,7 +2,7 @@
 
 import FrameLink from "./FrameLink";
 import { useState } from "react";
-import { useQuery } from "@tanstack/react-query";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import { getNearbyFrames } from "~/api";
 
 type Props = {
@@ -14,12 +14,16 @@ export default function NearbySelector({ episode, currentTimestamp }: Props) {
     const [nearbyTimestamp, setNearbyTimestamp] = useState<number>(currentTimestamp)
     const { data } = useQuery({ 
         queryKey: ['nearby', episode, nearbyTimestamp],
-        queryFn: async () => getNearbyFrames(episode, nearbyTimestamp)
+        queryFn: async () => getNearbyFrames(episode, nearbyTimestamp),
+        placeholderData: keepPreviousData
     })
+
+    if (!data) return null
 
     return (
         <div className="flex flex-row gap-4">
-            {data?.map((frame) => (
+            <button onClick={() => setNearbyTimestamp(data[0].timestamp)}>&lt;</button>
+            {data.map((frame) => (
                 <FrameLink 
                     key={frame.id} 
                     episode={frame.episode} 
@@ -27,6 +31,7 @@ export default function NearbySelector({ episode, currentTimestamp }: Props) {
                     className={frame.timestamp !== currentTimestamp ? "filter grayscale hover:grayscale-0" : ''}
                 />
             ))}
+            <button onClick={() => setNearbyTimestamp(data[data.length - 1].timestamp)}>&gt;</button>
         </div>
     )
 }
