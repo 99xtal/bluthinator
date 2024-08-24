e73866d18d52118540c0b5a9d438ab7e811aab1b
Joe Rybarczyk <joerybarczyk99@gmail.com>
2024-08-24 00:49:23 -0400

    feat(web): add footer with shameless self promotion

--- web/src/app/components/Logo.tsx
+++ web/src/app/components/Logo.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import TitleText from "../elements/TitleText";
 
 export default function Logo() {
-    const [isLargeScreen, setIsLargeScreen] = useState(false);
+    const [isLargeScreen, setIsLargeScreen] = useState(() => window.matchMedia('(min-width: 640px)').matches);
 
     useEffect(() => {
         const mediaQuery = window.matchMedia('(min-width: 640px)');
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -10,14 +10,21 @@ export default async function Home({ searchParams}: { searchParams: { q?: string
   const data = await search(query);
 
   return (
-    <div className="flex justify-center">
-      <div className="grid grid-cols-2 lg:grid-cols-3 gap-0 lg:gap-2">
-        {data.map((result: any, i: number) => (
-          <div key={result.timestamp + result.episode + i} className="p-1">
-            <FrameLink episode={result.episode} timestamp={result.timestamp} size="medium" />
-          </div>
-        ))}
+    <div className="flex flex-col min-h-screen">
+      <div className="flex-grow flex justify-center">
+        <div className="grid grid-cols-2 lg:grid-cols-3 gap-0 lg:gap-2">
+          {data.map((result: any, i: number) => (
+            <div key={result.timestamp + result.episode + i} className="p-1">
+              <FrameLink episode={result.episode} timestamp={result.timestamp} size="medium" />
+            </div>
+          ))}
+        </div>
       </div>
+      <footer className="bg-white py-4">
+        <div className="container mx-auto flex justify-center text-center text-gray-500 text-sm">
+          Created By&nbsp;<a href="https://www.99xtal.com" className="underline">7\</a>
+        </div>
+      </footer>
     </div>
   );
 }
