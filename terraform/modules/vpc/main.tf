b270d80c4798c479eba66f1dbcaa1fd05d2f64d1
Joe Rybarczyk <joerybarczyk99@gmail.com>
2024-08-28 22:11:41 -0400

    feat(terraform): add NAT gateway

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -55,3 +55,35 @@ resource "aws_route_table_association" "public_subnet_associations" {
   subnet_id      = aws_subnet.public_subnets[count.index].id
   route_table_id = aws_route_table.route_table.id
 }
+
+resource "aws_eip" "nat_eip" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "nat_gateway" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnets[0].id
+
+  tags = {
+    Name = "Bluthinator NAT Gateway"
+  }
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gateway.id
+  }
+
+  tags = {
+    Name = "Bluthinator Private Route Table"
+  }
+}
+
+resource "aws_route_table_association" "private_subnet_associations" {
+  count          = length(aws_subnet.private_subnets)
+  subnet_id      = aws_subnet.private_subnets[count.index].id
+  route_table_id = aws_route_table.private_route_table.id
+}
