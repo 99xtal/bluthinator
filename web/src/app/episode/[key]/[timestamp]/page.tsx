6b524ed19961384925c33d736ab37e33ddac6637
Joe Rybarczyk <joerybarczyk99@gmail.com>
2024-09-04 21:55:47 -0400

    feat(web): add opengraph metadata tags to episode frame page

--- web/src/app/episode/[key]/[timestamp]/page.tsx
+++ web/src/app/episode/[key]/[timestamp]/page.tsx
@@ -1,8 +1,31 @@
 import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
+import { Metadata } from "next";
 import { getFrame, getNearbyFrames } from "~/api";
 import FrameEditor from "~/app/components/FrameEditor";
-import FrameLink from "~/app/components/FrameLink";
 import NearbySelector from "~/app/components/NearbySelector";
+
+interface Props {
+  params: { key: string, timestamp: string }
+}
+
+export async function generateMetadata(
+  { params }: Props,
+): Promise<Metadata> {
+  return {
+    openGraph: {
+      title: "Bluthinator",
+      description: "An Arrested Development search engine and meme generator",
+      images: [
+        {
+          url: `${process.env.IMG_HOST}/frames/${params.key}/${params.timestamp}/large.jpg`,
+          width: 720,
+          height: 405,
+          alt: "Bluthinator image",
+        },
+      ]
+    }
+  }
+}
 
 export default async function Page({ params }: { params: { key: string, timestamp: string } }) {
   const data = await getFrame(params.key, params.timestamp);
